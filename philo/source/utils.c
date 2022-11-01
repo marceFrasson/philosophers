7b7d8fd6ee24a5fb7a2e5db34a60cb22712e2ae8
Marce <lightsxworld@gmail.com>
2022-11-01 02:42:12 -0300

    fsanitize warning

---
+++ philo/source/utils.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfrasson <mfrasson@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/31 12:12:07 by mfrasson          #+#    #+#             */
+/*   Updated: 2022/11/01 02:09:55 by mfrasson         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo.h"
+
+int	ft_atoi(const char *str)
+{
+	unsigned int	i;
+	double			num;
+	int				sinal;
+
+	i = 0;
+	num = 0;
+	sinal = 1;
+	while (str[i] == '\n' || str[i] == '\t' || str[i] == '\v'
+		|| str[i] == '\r' || str[i] == '\f' || str[i] == ' ')
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sinal *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		num = (num * 10) + str[i] - '0';
+		i++;
+	}
+	return (sinal * num);
+}
